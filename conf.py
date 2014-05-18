c9b12eec3b9e98bef7b68b0ca7d3b2f916b86bba
Minke Zhang <minke.zhang@gmail.com>
2014-05-18 16:09:51 -0500

    changed conf

--- conf.py
+++ conf.py
@@ -279,7 +279,15 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = [
+	'nikola github_deploy',
+	'git checkout master',
+	'echo "blog.yellowco.in" > CNAME',
+	'git add CNAME',
+	'git commit -am "added CNAME"',
+	'git push',
+	'git checkout source'
+]
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
